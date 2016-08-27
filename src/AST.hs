e4f11fc1a46133b55f7023cf6b2322e2d8069f8d
Daniel Mescheder <daniel.mescheder@realimpactanalytics.com>
2016-08-28 01:53:38 +0200

    ADD documentation

--- src/AST.hs
+++ src/AST.hs
@@ -19,7 +19,7 @@ import Data.Foldable
 import Data.Traversable
 
 
--- | The definition of a term in the language
+-- | The definition of a term in the sulfur language
 --
 --
 data Term a
@@ -32,7 +32,7 @@ data Term a
     deriving (Eq,Ord,Show,Read,Functor,Foldable,Traversable)
 
 
--- |
+-- | The definition of a pattern in the sulfur language
 data Pattern f a = VarPattern
                  | WildcardPattern
                  | AsPattern (Pattern f a)
@@ -66,37 +66,56 @@ instance Monad Term where
     (Typed typeTerm valueTerm) >>= f = Typed (typeTerm >>= f) (valueTerm >>= f)
     (Define e) >>= f =  Define ( e >>>= f)
 
+-- | A simplified constructor for lambda expressions in the sulfur language
+--
+-- A lambda expression consists of a pattern on the left hand side of the arrow and an expression on the right hand
+-- side.
 lambda :: Eq a => (PatternBuilder a) -> Term a -> Term a
 lambda (PatternBuilder buildPattern bindings) expression =
     Lam (length bindings) (buildPattern []) (abstract (`elemIndex` bindings) expression)
 
+-- | A simplified constructor for definitions in the sulfur language
+--
+-- A definition takes a symbol name, an optional type term and an expression for the value term.
+-- Semantically, the type of the value term should be compatible with the type term. The value term can be considered
+-- "bound" to the symbol name inside the current scope.
 define :: Eq a => a -> (Maybe (Term a)) -> (Term a) -> Term a
 define var (Just typeTerm) valueTerm = Define $ abstract1 var (Typed typeTerm valueTerm)
 define var (Nothing) valueTerm = Define $ abstract1 var valueTerm
 
+-- | A simplified constructor for patterns
+--
+-- In the Sulfur language, patterns form the left hand side of a lambda expression.
 data PatternBuilder a = PatternBuilder {
         buildPattern :: [a] -> Pattern Term a,
         bindings :: [a]
     }
 
+-- | Create a new PatternBuilder for a pattern that matches the entire expression and captures it in the symbol passed
+-- | as the first argument.
 varPattern :: a -> PatternBuilder a
 varPattern a = PatternBuilder {
     buildPattern = const VarPattern,
     bindings = [a]
 }
 
+-- | Create a new PatternBuilder for a pattern that matches everything and does not capture anything.
 wildcardPattern :: PatternBuilder a
 wildcardPattern = PatternBuilder {
     buildPattern = const WildcardPattern,
     bindings = []
 }
 
+-- | Create a new PatternBuilder that matches whatever the given sub-pattern matches and captures the entire expression
+-- | under the symbol passed as the first argument.
 asPattern :: a -> PatternBuilder a -> PatternBuilder a
 asPattern a (PatternBuilder p as) = PatternBuilder {
     buildPattern = \ bs -> AsPattern ( p ( a:bs ) ),
     bindings = (a:as)
 }
 
+-- | Create a new PatternBuilder that matches a given data constructor with a list of subpatterns for each of the
+-- | the constructor arguments.
 constructorPattern :: String -> [PatternBuilder a] -> PatternBuilder a
 constructorPattern constructorName subPatterns = PatternBuilder {
    buildPattern = \bs -> ConstructorPattern constructorName (buildSubPatterns subPatterns bs),
